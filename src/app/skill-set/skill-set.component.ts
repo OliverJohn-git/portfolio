5af4ad431a5565146fca9b401c9868f58cae73bc
OliverJohn-git <johnoliver846@gmail.com>
2024-12-22 02:33:54 +0100

    skills rename

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -8,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class SkillSetComponent {
 
+skills=[
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"HTML",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"CSS",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"JavaScript",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Material Design",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"TypeScript",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Angular",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Firebase",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Git",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Rest-Api",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Scrum",
+  },
+  {
+    image:"/assets/img/skill-set/html.png",
+    name:"Growth mindset",
+  },
+]
+
 }
